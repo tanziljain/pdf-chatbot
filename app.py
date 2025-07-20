157742889be0df4b9afb41e3fdab5d0e147d0b5d
Tanzil Jain <141444654+tanziljain@users.noreply.github.com>
2025-07-20 13:10:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 from dotenv import load_dotenv
 from langchain_community.vectorstores import FAISS
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.chains import RetrievalQA
 from langchain_groq import ChatGroq
 
